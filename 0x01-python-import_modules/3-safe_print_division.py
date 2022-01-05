cf80ac884348818dc141680094bb041d5ed86d15
CameronCrupper <guitardude20795@yahoo.com>
2022-01-05 22:19:04 +0000

    final result

--- 0x01-python-import_modules/3-safe_print_division.py
+++ 0x01-python-import_modules/3-safe_print_division.py
@@ -6,5 +6,5 @@ def safe_print_division(a, b):
         result = None
         return result
     finally:
-        print("Inside result: {}".format(res))
+        print("Inside result: {}".format(result))
         return result
