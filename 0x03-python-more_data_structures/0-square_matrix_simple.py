3a8b90739be5f2c299279bf4f9953b97e2fbded3
CameronCrupper <guitardude20795@yahoo.com>
2022-01-10 17:36:32 +0000

    test

--- 0x03-python-more_data_structures/0-square_matrix_simple.py
+++ 0x03-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return map(lambda x: x ** 2, matrix)
+    result = [[]]
+    for sublist in matrix:
+        row = []
+        for item in sublist:
+            row.append(item**2)
+        if result == [[]]:
+            result[0] = row
+        else:
+            result.append(row)
+    return result
