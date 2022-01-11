6d76125feaa2e591b53e63da524f9120a20665ea
CameronCrupper <guitardude20795@yahoo.com>
2022-01-11 20:10:34 +0000

    test

--- 0x04-python-classes/2-square.py
+++ 0x04-python-classes/2-square.py
@@ -11,7 +11,7 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-        
+
     def area(self):
         sq_area = self.__size * self.__size
         return(sq_area)
