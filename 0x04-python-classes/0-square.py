9ca95ab6e0bcfbfbadecef03f6394bb95021ed71
CameronCrupper <guitardude20795@yahoo.com>
2022-01-11 18:15:54 +0000

    test

--- 0x04-python-classes/0-square.py
+++ 0x04-python-classes/0-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """this module is creating a class called square"""
 class Square:
-    """This class is instantiated with size that has no value verification or type"""
+    """This class is instantiated size with no value verifi or type"""
     def __init__(self, size):
         self.__size = size
