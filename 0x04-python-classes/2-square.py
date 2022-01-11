497ffa6a7ba46904170cec2b6661dae5df978428
CameronCrupper <guitardude20795@yahoo.com>
2022-01-11 20:09:18 +0000

    test

--- 0x04-python-classes/2-square.py
+++ 0x04-python-classes/2-square.py
@@ -5,12 +5,13 @@
 class Square:
     """This class is instantiated size with no value verifi or type"""
 
-    def __init__(self, size=0):  
+    def __init__(self, size=0):
         if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+        
     def area(self):
         sq_area = self.__size * self.__size
         return(sq_area)
