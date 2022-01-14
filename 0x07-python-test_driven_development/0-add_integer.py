d6ad8fb6e65575b18b7b2275d491c57f7b63e132
CameronCrupper <guitardude20795@yahoo.com>
2022-01-14 17:35:33 +0000

    test cause who knows

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+A function that adds two integers
+both integers must be not float
+python3 -c 'print(__import__("my_module").__doc__)'
+"""
+
+def add_integer(a, b=98):
+
+    """
+if a or b are float change and return int
+"""
+
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    elif not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    else:
+        return int(a) + int(b)
