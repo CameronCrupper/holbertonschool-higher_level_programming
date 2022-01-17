6b65c9a9fb98bef59dd71cb70be840bcc1ef5a1c
CameronCrupper <guitardude20795@yahoo.com>
2022-01-17 16:46:32 +0000

    test

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -10,11 +10,9 @@ no importing of modules
 
 def say_my_name(first_name, last_name=""):
 
-
     """
     function that prints My name is <first name> <last name>
     """
-
 
     if type(first_name) != str:
         raise TypeError("first_name must be a string")
