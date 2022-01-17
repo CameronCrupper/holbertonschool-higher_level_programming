606c4f685ea779b178668f6d55aff31e3f94dea3
CameronCrupper <guitardude20795@yahoo.com>
2022-01-17 20:21:02 +0000

    tests

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -2,16 +2,16 @@
 
 
 """
-stuf
-an
-things
+text must be a string
+no spaces at the beginning or end of printed line
+no importing of modules
 """
 
 
 def text_indentation(text):
 
     """
-    stuff
+    print text w/ 2 new lines after each character . ? :
     """
 
     if not isinstance(text, str):
