a6d79864a97e6b40d68bd645f83a4b1a495086b7
CameronCrupper <guitardude20795@yahoo.com>
2022-01-17 16:42:10 +0000

    test

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+size is the length of the square
+size must be an integer
+size must be more than 0
+"""
+
+def print_square(size):
+    """
+    function that prints a square
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise TypeError("size must be >= 0")
+    if size == 0:
+        print("", end="")
+    else:
+        print("\n".join(["#" * size for rows in range(size)]))
