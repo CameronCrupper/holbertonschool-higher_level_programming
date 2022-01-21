e61870d1c92d07e20d85781229f491ac3309fe6b
CameronCrupper <guitardude20795@yahoo.com>
2022-01-21 03:32:56 +0000

    test

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -6,10 +6,10 @@ and methids of an object
 python3 -c 'print(__import__("my_module").__doc__)'
 """
 
+
 def lookup(obj):
 
     """
     python3 -c 'print(__import__("my_module").my_function.__doc__)'
-    python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
     """
     return dir(obj)
