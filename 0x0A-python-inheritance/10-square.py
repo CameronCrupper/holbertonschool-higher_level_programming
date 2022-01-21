08c4bd7dedace75ccfac4bf4fa84afff8503ea5b
CameronCrupper <guitardude20795@yahoo.com>
2022-01-21 04:10:44 +0000

    test

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -9,8 +9,7 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
     """
-    python3 -c 'print(__import__("my_module").my_function.__doc__)'
-    """
+    python3 -c 'print(__import__("my_module").my_function.__doc__)'"""
     def __init__(self, size):
         """
         python3 -c 'print(__import__("my_module").
@@ -23,13 +22,11 @@ class Square(Rectangle):
     def __str__(self):
         """
         python3 -c 'print(__import__("my_module").
-        MyClass.my_function.__doc__)'
-        """
+        MyClass.my_function.__doc__)'"""
         return ("[Rectangle] {}/{}".format(self.__width, self.__height))
 
     def area(self):
         """
         python3 -c 'print(__import__("my_module").
-        MyClass.my_function.__doc__)'
-        """
+        MyClass.my_function.__doc__)'"""
         return self.__size * self.__size
