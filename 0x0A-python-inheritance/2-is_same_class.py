59c228ddc9d84f828f7b6e09069e0201ccc409cc
CameronCrupper <guitardude20795@yahoo.com>
2022-01-21 03:36:27 +0000

    test

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
 
+
+"""
+python3 -c 'print(__import__("my_module").__doc__)'
+"""
 def is_same_class(obj, a_class):
+
+    """
+    python3 -c 'print(__import__("my_module").MyClass.__doc__)'
+    """
     return type(obj) == a_class
