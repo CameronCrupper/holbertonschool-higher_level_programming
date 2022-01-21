f03eb59ae771aa1e93479b29d9fdbd534454a0ae
CameronCrupper <guitardude20795@yahoo.com>
2022-01-21 03:40:29 +0000

    test

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
 
+
+"""
+python3 -c 'print(__import__("my_module").__doc__)'
+"""
+
+
 def inherits_from(obj, a_class):
+
+    """
+    python3 -c 'print(__import__("my_module").MyClass.__doc__)'
+    """
     return issubclass(type(obj), a_class) and type(obj) != a_class
