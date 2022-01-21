87bf6191b547195c35b7e67b7d9566e2a24cd554
CameronCrupper <guitardude20795@yahoo.com>
2022-01-21 19:13:06 +0000

    test

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
 
+"""
+python3 -c 'print(__import__("my_module").__doc__)'
+"""
+
+
 import json
 
 def to_json_string(my_obj):
 
+    """
+    python3 -c 'print(__import__("my_module").my_function.__doc__)'
+    """
     return json.dumps(my_obj)
