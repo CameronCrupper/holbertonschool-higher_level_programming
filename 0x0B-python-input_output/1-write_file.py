7b634bf8005b0a52d3f9c7650c01b6de7472b549
CameronCrupper <guitardude20795@yahoo.com>
2022-01-21 19:10:45 +0000

    test

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 
+"""
+python3 -c 'print(__import__("my_module").__doc__)'
+"""
+
+
 def write_file(filename="", text=""):
+    """
+    python3 -c 'print(__import__("my_module").my_function.__doc__)'
+    """
     with open(filename, mode="w", encoding="utf-8") as f:
 
         return f.write(text)
