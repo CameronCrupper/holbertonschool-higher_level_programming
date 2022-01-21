680821c5bdf3f6a3a4398bd5a64ae606a30caad0
CameronCrupper <guitardude20795@yahoo.com>
2022-01-21 19:07:52 +0000

    test

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 
-def read_file(filename=""):
+"""
+python3 -c 'print(__import__("my_module").__doc__)'
+"""
 
+
+def read_file(filename=""):
+    """
+    python3 -c 'print(__import__("my_module").my_function.__doc__)'
+    """
     with open("my_file_0.txt", encoding="utf-8") as filename:
 
         print(filename.read())
