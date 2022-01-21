639e33869c15b1d83114d2951116bba6ba5dbf4e
CameronCrupper <guitardude20795@yahoo.com>
2022-01-21 19:25:14 +0000

    test

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+python3 -c 'print(__import__("my_module").__doc__)'
+"""
+
+import json
+
+
+def from_json_string(my_str):
+    """
+    python3 -c 'print(__import__("my_module").my_function.__doc__)'
+    """
+    return json.loads(my_str)
