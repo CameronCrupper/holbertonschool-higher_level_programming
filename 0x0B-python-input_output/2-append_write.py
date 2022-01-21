9d3e6f2dc36ca0aee4228081535c87a43d8422a1
CameronCrupper <guitardude20795@yahoo.com>
2022-01-21 19:11:59 +0000

    test

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 
-def append_write(filename="", text=""):
+"""
+python3 -c 'print(__import__("my_module").__doc__)'
+"""
 
+
+def append_write(filename="", text=""):
+    """
+    python3 -c 'print(__import__("my_module").my_function.__doc__)'
+    """
     with open(filename, mode="a", encoding="utf-8") as f:
 
         return f.write(text)
