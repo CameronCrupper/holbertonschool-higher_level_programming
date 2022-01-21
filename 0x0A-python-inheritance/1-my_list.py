52146bb5a52fff7e38850026a572f8fb2020079e
CameronCrupper <guitardude20795@yahoo.com>
2022-01-21 04:50:13 +0000

    test

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,28 +1,21 @@
 #!/usr/bin/python3
-
-
 """
 python3 -c 'print(__import__("my_module").__doc__)'
 """
 
 
 class MyList(list):
-
     """
     python3 -c 'print(__import__("my_module").MyClass.__doc__)'
     """
-
     def __init__(self):
         """
-        python3 -c 'print(__import__("my_module").my_function.__doc__)'
         python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
         """
         super().__init__()
 
     def print_sorted(self):
-
         """
-        python3 -c 'print(__import__("my_module").my_function.__doc__)'
         python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
         """
         print(sorted(self))
