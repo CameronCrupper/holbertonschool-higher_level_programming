13272c108984eaff0fc3ca3eea44f1481f8508bd
CameronCrupper <guitardude20795@yahoo.com>
2022-01-21 03:39:04 +0000

    test

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
 
+
+"""
+python3 -c 'print(__import__("my_module").__doc__)'
+"""
+
+
 def is_kind_of_class(obj, a_class):
+
+    """
+    python3 -c 'print(__import__("my_module").MyClass.__doc__)'
+    """
+
     if isinstance(obj, a_class):
         return True
     else:
