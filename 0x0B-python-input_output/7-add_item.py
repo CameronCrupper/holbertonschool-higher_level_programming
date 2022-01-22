2895216aa02161e8261719552701879abbdd28d8
CameronCrupper <guitardude20795@yahoo.com>
2022-01-22 03:51:52 +0000

    test

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+python3 -c 'print(__import__("my_module").__doc__)'
+"""
+
+import json
+from sys import argv
+
+
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+try:
+    new = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    new = []
+
+for arg in argv[1:]:
+    list.append(arg)
+
+save_to_json_file(list, "add_item.json")
