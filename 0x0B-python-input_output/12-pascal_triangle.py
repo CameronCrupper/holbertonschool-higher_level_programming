8f0888eb5c827f338567bc6afb1be06017d15b3e
CameronCrupper <guitardude20795@yahoo.com>
2022-01-22 04:03:47 +0000

    test

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -2,11 +2,13 @@
 
 
 """
-
+python3 -c 'print(__import__("my_module").__doc__)'
 """
 
 
 def pascal_triangle(n):
+    """
+    python3 -c 'print(__import__("my_module").my_function.__doc__)'
+    """
     if n <= 0
         return []
-    
