a439df7bc4460b7dbbf6b5e2c577e8495f393bf1
root <3626@holbertonschool.com>
2022-02-09 13:39:45 -0800

    test

--- 0x00-SQL_introduction/7-insert_value.sql
+++ 0x00-SQL_introduction/7-insert_value.sql
@@ -2,4 +2,4 @@
     -- New row
         -- id = 89
         -- name = Best School
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Holberton School")
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School")
