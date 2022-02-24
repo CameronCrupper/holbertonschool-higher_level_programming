e1478b786a00343bdfa59bb967dda0695069eaff
root <3626@holbertonschool.com>
2022-02-24 08:32:33 -0800

    test

---
+++ 0x0B-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+print the first Stateobject from the database
+hbtn_0e_6_usa
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sys import argv
+
+if __name__ == "__main__":
+    """
+    
+    """
+    datab = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3])
+    engine = create_engine(datab)
+    Session = sessionmaker(bind=engine)
+    sesh = Session()
+    for isinstance in sesh.query(State).filter(State.name.contains("a")):
+        print("{0}: {1}".format(isinstance.id, isinstance.name))
