18cfbcd7194da9c21c0cf16b5bacf63e0eb3bef5
root <3626@holbertonschool.com>
2022-02-24 12:31:23 -0800

    test

--- 0x0B-python-object_relational_mapping/6-model_state.py
+++ 0x0B-python-object_relational_mapping/6-model_state.py
@@ -13,11 +13,15 @@ if __name__ == "__main__":
     State class:
     inherits from Base Tips
     links to the MySQL table states
-    class attribute id that represents a column of an auto-generated, unique integer, cant be null and is a primary key
-    class attribute name that represents a column of a string with maximum 128 characters and cant be null
+    class attribute id that represents a column of
+    an auto-generated, unique integer, cant be null
+    and is a primary key class attribute name that represents
+    a column of a string with maximum 128 characters and cant be null
     You must use the module SQLAlchemy
-    Your script should connect to a MySQL server running on localhost at port 3306
-    WARNING: all classes who inherit from Base must be imported before calling Base.metadata.create_all(engine)
+    Your script should connect to a MySQL server running on
+    localhost at port 3306
+    WARNING: all classes who inherit from Base must be
+    imported before calling Base.metadata.create_all(engine)
     """
-        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-        Base.metadata.create_all(engine)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
