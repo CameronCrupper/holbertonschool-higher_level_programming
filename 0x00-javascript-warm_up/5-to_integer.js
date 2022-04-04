e5c143e036e6a42ea0868769fb0b87e580317a51
Cameron.Crupper <guitardude20795@yahoo.com>
2022-04-04 11:35:23 -0700

    test

--- 0x00-javascript-warm_up/5-to_integer.js
+++ 0x00-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const test = parseInt(process.argv[2])
+const test = parseInt(process.argv[2]);
 if (test) {
   console.log('My number: ' + test);
 } else {
-    console.log('Not a number');
+  console.log('Not a number');
 }
