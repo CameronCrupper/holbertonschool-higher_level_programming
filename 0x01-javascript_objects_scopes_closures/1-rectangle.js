a26c52eb3ca89be3a3351cbef64358be9ad9856a
Cameron.Crupper <guitardude20795@yahoo.com>
2022-04-05 10:12:38 -0700

    test

---
+++ 0x01-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+//define rectangle width and height
+module.exports = class Rectangle {
+  constructor (width, height) {
+    this.width = width;
+    this.height = height;
+  }
+};
