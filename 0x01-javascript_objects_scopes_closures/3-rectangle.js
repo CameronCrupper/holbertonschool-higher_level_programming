ca40f8e2b40289584d0094ccaf01e5dc2944325b
Cameron.Crupper <guitardude20795@yahoo.com>
2022-04-05 11:52:43 -0700

    test

---
+++ 0x01-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (width, height) {
+    if (typeof width === 'number' && typeof height === 'number' && width > 0 && height > 0) {
+      this.width = width;
+      this.height = height;
+    }
+  }
+  print () {
+    for (let j = 0; j < this.height; j++) {
+      let k = 0;
+      for (; k < this.width; k++) {
+        process.stdout.write('X');
+      }
+      if (k === this.width) {
+        console.log('');
+      }
+    }
+  }
+};
